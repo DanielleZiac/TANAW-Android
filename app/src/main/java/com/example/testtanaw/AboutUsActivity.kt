f0a70417782422d4d63698250ff6fd206740d5d8
cerphh <curvesgt@gmail.com>
2024-12-06 11:54:21 +0800

    aboutus/insti

--- app/src/main/java/com/example/testtanaw/AboutUsActivity.kt
+++ app/src/main/java/com/example/testtanaw/AboutUsActivity.kt
@@ -1,13 +1,24 @@
 package com.example.testtanaw
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class AboutUsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_us)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Set the toolbar title to be centered
+        supportActionBar?.setDisplayShowTitleEnabled(false) // Disable default title
+        val titleTextView = toolbar.findViewById<TextView>(R.id.toolbar_title)
+
+        // Enable the back button in the toolbar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         // Additional setup can go here if needed.
     }
 }
--- app/src/main/java/com/example/testtanaw/LeaderboardFragment.kt
+++ app/src/main/java/com/example/testtanaw/LeaderboardFragment.kt
@@ -1,16 +1,43 @@
 package com.example.testtanaw
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testtanaw.models.LeaderboardItem
 
 class LeaderboardFragment : Fragment() {
+
+    private lateinit var leaderboardRecyclerView: RecyclerView
+    private lateinit var leaderboardAdapter: LeaderboardAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_leaderboard, container, false)
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_leaderboard, container, false)
+
+        // Initialize RecyclerView
+        leaderboardRecyclerView = rootView.findViewById(R.id.leaderboardRecyclerView)
+        leaderboardRecyclerView.layoutManager = LinearLayoutManager(context)
+        leaderboardRecyclerView.setHasFixedSize(true)
+
+        // Example data, in a real case this would come from a backend
+        val leaderboardList = listOf(
+            LeaderboardItem("User1", 50),
+            LeaderboardItem("User2", 45),
+            LeaderboardItem("User3", 40),
+
+            )
+
+        // Set the Adapter
+        leaderboardAdapter = LeaderboardAdapter(leaderboardList)
+        leaderboardRecyclerView.adapter = leaderboardAdapter
+
+        return rootView
     }
 }
