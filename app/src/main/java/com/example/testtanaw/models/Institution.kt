81862a68cd0c93e80120a22a4ae03eed8b316aa0
Danielle Ziac <danielle.abrilx@gmail.com>
2024-12-07 02:17:58 +0800

    insitution

--- app/src/main/java/com/example/testtanaw/models/Institution.kt
+++ app/src/main/java/com/example/testtanaw/models/Institution.kt
@@ -1,8 +1,8 @@
 package com.example.testtanaw.models
 
 data class Institution(
-    val name: String,          // Name of the institution
-    val description: String,   // Description or full name
+    val institution: String,          // Name of the institution
+    val campus: String,   // Description or full name
     val logoResId: Int,
     )
 
